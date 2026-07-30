f7bfe55d2df073bfa23f2384e08cea29aafd03b0
nunovinhas-creator <278368112+nunovinhas-creator@users.noreply.github.com>
2026-07-30 10:51:53 +0000

    fix: add root path to sys.path for streamlit

--- scripts/app.py
+++ scripts/app.py
@@ -1,3 +1,10 @@
+import sys
+from pathlib import Path
+
+root_path = Path(__file__).resolve().parent.parent
+if str(root_path) not in sys.path:
+    sys.path.insert(0, str(root_path))
+
 import streamlit as st
 import pandas as pd
 import sqlite3
